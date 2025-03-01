d1900f57a2489873afdf732fcbc43beb916d68c9
Edward <edwardboere@gmail.com>
2025-03-01 12:52:50 +0100

    add fibonacci test

---
+++ tests/test_fibonacci.py
@@ -0,0 +1,14 @@
+from sequence.fibonacci import fibonacci
+import pytest
+
+
+def test_fibonacci():
+    assert fibonacci(1) == 0
+    assert fibonacci(2) == 1
+    assert fibonacci(5) == 3
+    assert fibonacci(10) == 34
+
+
+def test_fibonacci_invalid():
+    with pytest.raises(ValueError):
+        fibonacci(0)
